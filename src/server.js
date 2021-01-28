6efaa6ba169cd04f9470b36cde81137cc31af1cf
LautaroTiamat <lautarotiamat@gmail.com>
2021-01-28 20:05:46 -0300

    upload files 2

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const PHPServer = require('php-server-manager')
-const express = require('express')
-const app = express()
+//const express = require('express')
+//const app = express()
  
 const server = new PHPServer({
-    port: 8000,
+    //port: 8000,
     //host: 'localhost',
     directory: 'src/public-page',
     //script: 'src/public-page/index.php',
@@ -15,10 +15,10 @@ const server = new PHPServer({
 
 server.run()
 
-app.get('/', function(req, res){
+/*app.get('/', function(req, res){
     res.send('Hi!')
 })
 
 app.listen('5000', function() {
     console.log('Servidor web escuchando en el puerto 5000')
-})
+})*/
